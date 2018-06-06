40a7d78b8418a1d461ce75e2de619e53230e90bd
Anton Österberg <anton.osterberg@gmail.com>
2018-06-06 20:56:15 +0200

    Change name of enum Algorithm to HashAlgorithm

--- src/main/kotlin/Hash.kt
+++ src/main/kotlin/Hash.kt
@@ -3,8 +3,7 @@ package com.mantono.pyttipanna
 import java.nio.charset.Charset
 import java.security.MessageDigest
 
-
-enum class Algorithm(val hashFunction: String, val instance: MessageDigest = MessageDigest.getInstance(hashFunction))
+enum class HashAlgorithm(val hashFunction: String, val instance: MessageDigest = MessageDigest.getInstance(hashFunction))
 {
 	MD5("MD5"),
 	SHA1("SHA-1"),
@@ -13,18 +12,18 @@ enum class Algorithm(val hashFunction: String, val instance: MessageDigest = Mes
 	SHA512("SHA-512")
 }
 
-fun hash(data: String, salt: String, algorithm: Algorithm = Algorithm.SHA512): ByteArray
+fun hash(data: String, salt: String, algorithm: HashAlgorithm = HashAlgorithm.SHA512): ByteArray
 {
 	return hash(data.toUtf8Bytes(), salt.toUtf8Bytes(), algorithm)
 }
 
-fun hash(data: String, algorithm: Algorithm = Algorithm.SHA512): ByteArray
+fun hash(data: String, algorithm: HashAlgorithm = HashAlgorithm.SHA512): ByteArray
 {
 	val utf8 = Charset.forName("UTF-8")
 	return hash(data.toByteArray(utf8), algorithm = algorithm)
 }
 
-fun hash(data: ByteArray, salt: ByteArray = emptyByteArray, algorithm: Algorithm = Algorithm.SHA512): ByteArray
+fun hash(data: ByteArray, salt: ByteArray = emptyByteArray, algorithm: HashAlgorithm = HashAlgorithm.SHA512): ByteArray
 {
 	val digest = algorithm.instance
 	digest.update(salt)
--- src/test/kotlin/HashTest.kt
+++ src/test/kotlin/HashTest.kt
@@ -10,7 +10,7 @@ class HashTest
 	@Test
 	fun testHashNoSalt()
 	{
-		val s = hash("test", Algorithm.SHA512)
+		val s = hash("test", HashAlgorithm.SHA512)
 		val x = s.toHex()
 
 		val hashSha512 = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff"
@@ -20,7 +20,7 @@ class HashTest
 	@Test
 	fun testHashEmptySalt()
 	{
-		val s = hash("test", "", Algorithm.SHA512)
+		val s = hash("test", "", HashAlgorithm.SHA512)
 		val x = s.toHex()
 
 		val hashSha512 = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff"
@@ -30,7 +30,7 @@ class HashTest
 	@Test
 	fun testHashWithSalt()
 	{
-		val s = hash("test", "some salt", Algorithm.SHA512)
+		val s = hash("test", "some salt", HashAlgorithm.SHA512)
 		val x = s.toHex()
 
 		val hashSha512 = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff"
