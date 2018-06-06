149017d8e083d35dcf2eab3f412fa184eedd2bde
Anton Österberg <anton.osterberg@gmail.com>
2018-06-06 21:31:05 +0200

    Create function secureHash that uses PBKDF2 for generating hash.

---
+++ src/main/kotlin/SecureHash.kt
@@ -0,0 +1,44 @@
+package com.mantono.pyttipanna
+
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+
+const val DEFAULT_HASH_ITERATIONS = 200
+const val DEFAULT_HASH_LENGTH = 64
+
+fun secureHash(
+		data: String,
+		salt: ByteArray,
+		iterations: Int = DEFAULT_HASH_ITERATIONS,
+		length: Int = DEFAULT_HASH_LENGTH
+): ByteArray = secureHash(data.toCharArray(), salt, iterations, length)
+
+fun secureHash(
+		data: ByteArray,
+		salt: ByteArray,
+		iterations: Int = DEFAULT_HASH_ITERATIONS,
+		length: Int = DEFAULT_HASH_LENGTH
+): ByteArray = secureHash(String(data).toCharArray(), salt, iterations, length)
+
+/**
+ * @param data the secret that should be hashed
+ * @param salt is the salt that is used to make the generated
+ * hash unique for the given input (_data_)
+ * @param iterations the number of iterations used when generating
+ * the hash. Default value is [DEFAULT_HASH_ITERATIONS]
+ * @param length of the generated hash, in bytes. Will throw an [IllegalArgumentException]
+ * if the number is too low to be considered secure. Currently this number is
+ * set to 8 bytes (64 bits) and default value is [DEFAULT_HASH_LENGTH]
+ */
+fun secureHash(
+		data: CharArray,
+		salt: ByteArray,
+		iterations: Int = DEFAULT_HASH_ITERATIONS,
+		length: Int = DEFAULT_HASH_LENGTH
+): ByteArray
+{
+	require(length >= 4) { "Length must be at least 4, but it was $length" }
+	val keySpec = PBEKeySpec(data, salt, iterations, length * 8)
+	val keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+	return keyFactory.generateSecret(keySpec).encoded
+}
--- src/test/kotlin/SecureHashTest.kt
+++ src/test/kotlin/SecureHashTest.kt
@@ -1,5 +1,88 @@
 package com.mantono.pyttipanna
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.time.Instant
+import java.util.*
+
+private val pass1 = "my secret password"
+private val pass2 = "my other secret password"
+private val salt1 = "some data to be used as salt".toByteArray()
+private val salt2 = "some other data to be used as salt".toByteArray()
+
 class SecureHashTest
 {
+	@Test
+	fun testSecureHashIsEqualsOnSameInputs()
+	{
+		val first: ByteArray = secureHash(pass1, salt1, 10, 30)
+		val second: ByteArray = secureHash(pass1, salt1, 10, 30)
+		assertArrayEquals(first, second)
+	}
+
+	@Test
+	fun testSecureHashIsNotEqualOnDifferentPassword()
+	{
+		val b0 = secureHash(pass1, salt1, 100, 30)
+		val b1 = secureHash(pass2, salt1, 100, 30)
+		assertArrayNotEquals(b0, b1)
+	}
+
+	@Test
+	fun testSecureHashIsNotEqualOnSameInputButDifferentSalt()
+	{
+		val b0 = secureHash(pass1, salt1, 100, 30)
+		val b1 = secureHash(pass1, salt2, 100, 30)
+		assertArrayNotEquals(b0, b1)
+	}
+
+	@Test
+	fun testSecureHashIsNotEqualsOnSameInputsButDifferentIterations()
+	{
+		val tenIterations: ByteArray = secureHash(pass1, salt1, 10, 30)
+		val elevenIterations: ByteArray = secureHash(pass1, salt1, 11, 30)
+		assertArrayNotEquals(tenIterations, elevenIterations)
+	}
+
+	@Test
+	fun testSecureHashIsNotEqualsOnSameInputsButDifferentLength()
+	{
+		val tenIterations: ByteArray = secureHash(pass1, salt1, 10, 30)
+		val elevenIterations: ByteArray = secureHash(pass1, salt1, 10, 31)
+		assertArrayNotEquals(tenIterations, elevenIterations)
+	}
+
+	@Test
+	fun testThatIterationsIncreasesHashingTime()
+	{
+		val faster: Long = clockTime { secureHash(pass1, salt1, 400) }
+		val slower: Long = clockTime { secureHash(pass1, salt1, 800) }
+		assertTrue(faster < slower) { "$faster >= $slower" }
+	}
+
+	@Test
+	fun testAssertCorrectLength()
+	{
+		assertLength(4)
+		assertLength(10)
+		assertLength(20)
+		assertLength(30)
+		assertLength(100)
+	}
+
+	@Test
+	fun testAssertThrowsOnTooShortLength()
+	{
+		assertThrows<IllegalArgumentException> {
+			assertLength(3)
+		}
+	}
+
+	private fun assertLength(length: Int)
+	{
+		assertEquals(length, secureHash(pass1, salt1, 20, length).size)
+	}
 }
